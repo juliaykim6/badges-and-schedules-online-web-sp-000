26984d1376562b288d3457f0d4b029d3e101617e
Julia Kim <juliaykim6@gmail.com>
2021-01-16 21:47:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(name, counter)
-  name.map {|name| << "Hello, #{name}! You'll be assigned to room #{counter}!"}
+  badge_maker(name)
 end
 
 
