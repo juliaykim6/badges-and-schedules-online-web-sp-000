3779979b3352f82663ce003c9cfc7ab9e781007a
Julia Kim <juliaykim6@gmail.com>
2021-01-16 21:45:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(names)
   names.map do |name|
-    "Hello, my name is #{name}."
+    "Hello, my name is #{names}."
   end
 end
 
